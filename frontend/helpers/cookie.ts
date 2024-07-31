29af5794752fc38f3529780535aa416c354281b5
rajbeer1 <royu49@gmail.com>
2024-08-01 00:42:41 +0530

    dd

--- frontend/helpers/cookie.ts
+++ frontend/helpers/cookie.ts
@@ -4,6 +4,8 @@ export const setCookie=(name: string, value: string, days: number) =>{
     const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = '; expires=' + date.toUTCString();
+
+    
   }
   document.cookie =
     name +
