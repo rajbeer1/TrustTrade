2350cf84b2e1a498ea17abd3a30ff97d358521d8
rajbeer1 <royu49@gmail.com>
2024-08-01 17:13:13 +0530

    fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import { config } from './config/config';
 import { app } from './app';
-import { prisma } from './helpers';
+
 config.verifyConfig()
     app.listen(config.PORT, () => {
       console.log(`Server running on port ${config.PORT}`);
     });
+ 
